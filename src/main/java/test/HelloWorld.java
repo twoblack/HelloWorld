ce994201c4b2be894d3e7146c1b671dc8575ce2b
liusong <liusong@gemdale.com>
2018-10-08 11:13:39 +0800

    1111

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -6,15 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorld {
 
-	public String sayHello(){
-		System.out.println("哈哈哈哈");
-		return "Hello World!";
-	}
-	
-	public String say(){
-		return "Hello";
-	}
-	
     public static void main(String[] args) {
         SpringApplication.run(HelloWorld.class, args);
     }
