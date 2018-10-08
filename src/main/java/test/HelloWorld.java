4c0f7591990a12ac74ba36da1df141792c324c6d
liusong <liusong@gemdale.com>
2018-10-08 09:03:49 +0800

    1

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -1,10 +1,14 @@
 package test;
 
 public class HelloWorld {
+	
+	public String sayHello(){
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
+		System.out.println(new HelloWorld().sayHello());
 	}
 
 }
