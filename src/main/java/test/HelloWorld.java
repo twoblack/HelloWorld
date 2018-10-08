435565a28295da63058c5ed6a015ece51359bbef
liusong <liusong@gemdale.com>
2018-10-08 10:03:52 +0800

    1

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -1,14 +1,17 @@
 package test;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HelloWorld {
-	
+
 	public String sayHello(){
 		return "Hello World!";
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(new HelloWorld().sayHello());
-	}
-
+	
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorld.class, args);
+        System.out.println(new HelloWorld().sayHello());
+    }
 }
